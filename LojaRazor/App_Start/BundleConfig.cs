45f16c7952906b2b252e8feb1c84c9010b8a59f7
David Pereira <d_pereira@outlook.com.br>
2018-07-24 21:41:38 -0300

    Utilizando Bundles

--- LojaRazor/App_Start/BundleConfig.cs
+++ LojaRazor/App_Start/BundleConfig.cs
@@ -8,7 +8,20 @@ namespace LojaRazor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            StyleBundle estilos = new StyleBundle("~/bundles/estilos");
+            ScriptBundle scripts = new ScriptBundle("~/bundles/scripts");
+
+            estilos.Include("~/Content/bootstrap/css/bootstrap.css");
+            estilos.Include("~/Content/Style.css");
+
+            scripts.Include("~/Scripts/jquery-1.7.1.js");
+            scripts.Include("~/Scripts/jquery.validade.js");
+            scripts.Include("~/Scripts/jquery.validade.unobtrusive.js");
+
+            bundles.Add(estilos);
+            bundles.Add(scripts);
+
+
         }
     }
 }
